83e2fa34a724e0a4fc5ba9339e8a90739c5dad7d
Freddy J Moreno B <freddy.moreno@gmail.com>
2022-11-05 19:30:44 -0300

    Base Project

--- examples/bootstrap/checkout/form-validation.js
+++ examples/bootstrap/checkout/form-validation.js
@@ -6,14 +6,18 @@
   const forms = document.querySelectorAll('.needs-validation')
 
   // Loop over them and prevent submission
-  Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-      }
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      'submit',
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
 
-      form.classList.add('was-validated')
-    }, false)
+        form.classList.add('was-validated')
+      },
+      false
+    )
   })
 })()
